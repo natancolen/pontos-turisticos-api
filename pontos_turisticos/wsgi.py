e9c80251efffa8eab936b6fca4fb41fe4a1466fb
natancolen <natancolen@gmail.com>
2023-01-29 18:52:51 -0300

    rest framework

--- pontos_turisticos/wsgi.py
+++ pontos_turisticos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pontosturististicos project.
+WSGI config for pontos_turisticos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontosturististicos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontos_turisticos.settings')
 
 application = get_wsgi_application()
